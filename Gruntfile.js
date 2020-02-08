498ff9fdb533c37ed044f09d3f1daa7424180fc0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-08 18:26:47 +0000

    Deploy deVinnnie/next-time to github.com/deVinnnie/next-time.git:gh-pages

---
+++ Gruntfile.js
@@ -0,0 +1,61 @@
+module.exports = function(grunt) {
+    
+    // Project configuration.
+    grunt.initConfig({
+        pug: {
+            children: {
+                options: {
+                    data: {
+                        debug: false
+                    }
+                },
+                files: [ {
+                    src: ["*/index.pug"],
+                    dest: "./",
+                    expand: true,
+                    ext: ".html"
+                } ]
+            },
+            index: {
+                options: {
+                    data: {
+                        debug: false,
+                        files: grunt.file.expand(
+                          {filter: "isFile"},
+                          [ "**/*.html", 
+                            "!index.html", 
+                            "!node_modules/**"
+                          ]
+                        )
+                    }
+                },
+                files: {
+                    'index.html' : 'index.pug'
+                }
+            }
+        },
+
+        less: {
+            all:{
+                files: {
+                    "css/main-2015-11.css": "less/main-2015-11.less",
+                    "css/main-2015-09.css": "less/main-2015-09.less",
+                    "css/main-2018-04.css": "less/main-2018-04.less"
+                }
+            }
+        },
+        connect : {
+            uses_defaults: {}
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-pug');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+
+    // Default task(s).
+    grunt.registerTask('default', ['pug:children', 'less']);
+    
+    // Default task(s).
+    grunt.registerTask('serve', ['connect:uses_defaults:keepalive']);
+};
